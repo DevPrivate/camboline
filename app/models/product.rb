97a6fb9e9fe96b5138f7d7916f5f7f5349fee920
kongou <oukong84@gmail.com>
2016-03-01 13:25:33 +0700

    Fix errror Dropbox

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,9 +4,9 @@ class Product < ActiveRecord::Base
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   else
     has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "missing.png",
-                      storage: :dropbox,
-                      dropbox_credentials: Rails.root.join("config/dropbox.yml"),
-                      path: ':style/:id_:filename'
+                      :storage => :dropbox,
+                      :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+                      :path => ':style/:id_:filename'
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   end
 end
