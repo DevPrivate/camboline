70446b16f15faa8bd41ba20c2414f0d88d040b09
kongou <oukong84@gmail.com>
2016-03-09 21:59:53 +0700

    Install and set up Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  root 'products#index'
+
+  devise_for :users
+
   resources :products
 
   get 'pages/about'
 
   get 'pages/contact'
-
-  root 'products#index'
-
 end
